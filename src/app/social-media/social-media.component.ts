5e00dcffb6dcae7fbc42ea6e05bd8c9ed9e914eb
Aurélien Guelle <aurelien.guelle@gmail.com>
2024-11-04 17:35:27 +0100

    add style to admin pages

--- src/app/social-media/social-media.component.ts
+++ src/app/social-media/social-media.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faTwitter, faDiscord } from '@fortawesome/free-brands-svg-icons'; // Importer uniquement les icônes nécessaires
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin, faInstagram, faXTwitter, faFacebook} from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -14,6 +14,6 @@ import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
 })
 export class SocialMediaComponent {
   constructor(library: FaIconLibrary) {
-    library.addIcons(faTwitter, faDiscord, faGithub, faLinkedin, faPhone, faEnvelope); // Ajouter uniquement les icônes spécifiques
+    library.addIcons(faTwitter, faDiscord, faGithub, faLinkedin, faPhone, faEnvelope, faInstagram, faXTwitter, faFacebook); // Ajouter uniquement les icônes spécifiques
   }
 }
